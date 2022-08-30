7f954ce13472002b449a68df8985902e63591ee8
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2022-08-30 20:37:13 +0400

    Add get_deposit_outputs function
    * Clarify memory management a bit
    * Add partial connect_block and disconnect_block implementation (now
      they only handle deposits)
    * Add free_string and free_deposits functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
+// TODO: Very carefully check that there are no memory management bugs.
 use drivechain::*;
+use std::collections::HashMap;
 use std::ffi::{CStr, CString};
 use std::str::FromStr;
 use std::sync::RwLock;
-
-#[no_mangle]
-pub extern "C" fn test_function() {}
 
 static mut DRIVECHAIN: Option<RwLock<Drivechain>> = None;
 
 #[no_mangle]
 pub unsafe extern "C" fn init(
+    // NOTE: The caller is responsible for freeing these strings after init is
+    // done.
     db_path: *const libc::c_char,
     this_sidechain: usize,
     rpcuser: *const libc::c_char,
@@ -21,6 +22,12 @@ pub unsafe extern "C" fn init(
     DRIVECHAIN = Drivechain::new(db_path, this_sidechain, rpcuser, rpcpassword)
         .map(RwLock::new)
         .ok();
+    dbg!(DRIVECHAIN
+        .as_ref()
+        .unwrap()
+        .read()
+        .unwrap()
+        .format_deposit_address("address"));
 }
 
 #[no_mangle]
@@ -80,6 +87,8 @@ pub unsafe extern "C" fn verify_bmm(
 }
 
 #[no_mangle]
+// NOTE: The caller is responsible for freeing the returned string with
+// free_string after a more memory safe string type is constructed.
 pub unsafe extern "C" fn get_prev_main_block_hash(
     main_block_hash: *const libc::c_char,
 ) -> *const libc::c_char {
@@ -94,11 +103,106 @@ pub unsafe extern "C" fn get_prev_main_block_hash(
         .get_prev_main_block_hash(&main_block_hash)
         .unwrap();
     let prev = CString::new(prev.to_string()).unwrap();
-    // Put prev on the heap, so it is not deallocated when the stack frame is dropped.
-    let prev = Box::new(prev);
-    // Move prev out of the Rust memory model, so it is not deallocated automatically.
-    let prev: *const CString = std::mem::transmute(prev);
-    // Now it is the responsibility of the caller to free this string after use
-    // (most likely after converting it into a more memory safe type).
-    (&*prev).as_ptr()
+    // NOTE: This string must be reconstructed back into CString to be freed.
+    // https://doc.rust-lang.org/alloc/ffi/struct.CString.html#method.into_raw
+    prev.into_raw()
+}
+
+#[repr(C)]
+pub struct Deposit {
+    pub address: *const libc::c_char,
+    pub amount: u64,
+}
+
+#[repr(C)]
+pub struct Deposits {
+    pub ptr: *mut Deposit,
+    pub len: usize,
+}
+
+#[no_mangle]
+// NOTE: The caller is responsible for freeing Deposits by calling free_deposits
+// after a more memory safe "Deposits" data structure is constructed.
+pub unsafe extern "C" fn get_deposit_outputs() -> Deposits {
+    let deposits = DRIVECHAIN
+        .as_ref()
+        .unwrap()
+        .read()
+        .unwrap()
+        .get_deposit_outputs()
+        .unwrap();
+    let deposits: Vec<Deposit> = deposits
+        .into_iter()
+        .map(|d| {
+            let address = CString::new(d.address).unwrap();
+            Deposit {
+                // NOTE: This string must be reconstructed back into CString to be freed.
+                // https://doc.rust-lang.org/alloc/ffi/struct.CString.html#method.into_raw
+                address: address.into_raw(),
+                amount: d.amount,
+            }
+        })
+        .collect();
+    let mut deposits = deposits.into_boxed_slice();
+    let result = Deposits {
+        ptr: deposits.as_mut_ptr(),
+        len: deposits.len(),
+    };
+    std::mem::forget(deposits);
+    result
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn connect_block(deposits: Deposits, just_check: bool) -> bool {
+    let deposits = std::slice::from_raw_parts(deposits.ptr, deposits.len);
+    let deposits: Vec<drivechain::Deposit> = deposits
+        .iter()
+        .map(|d| drivechain::Deposit {
+            address: CStr::from_ptr(d.address).to_str().unwrap().into(),
+            amount: d.amount,
+        })
+        .collect();
+    DRIVECHAIN
+        .as_mut()
+        .unwrap()
+        .write()
+        .unwrap()
+        .connect_block(&deposits, &HashMap::new(), &[], just_check)
+        .is_ok()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn disconnect_block(deposits: Deposits, just_check: bool) -> bool {
+    let deposits = std::slice::from_raw_parts(deposits.ptr, deposits.len);
+    let deposits: Vec<drivechain::Deposit> = deposits
+        .iter()
+        .map(|d| drivechain::Deposit {
+            address: CStr::from_ptr(d.address).to_str().unwrap().into(),
+            amount: d.amount,
+        })
+        .collect();
+    DRIVECHAIN
+        .as_mut()
+        .unwrap()
+        .write()
+        .unwrap()
+        .disconnect_block(&deposits, &[], &[], just_check)
+        .is_ok()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn free_string(string: *const libc::c_char) {
+    drop(CString::from_raw(string as *mut libc::c_char));
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn free_deposits(deposits: Deposits) {
+    // Convert raw pointer and length into &mut [Deposit].
+    let deposits = std::slice::from_raw_parts_mut(deposits.ptr, deposits.len);
+    // Free all address strings.
+    for deposit in deposits.iter() {
+        free_string(deposit.address);
+    }
+    // Free slice memory.
+    std::ptr::drop_in_place(deposits);
 }
